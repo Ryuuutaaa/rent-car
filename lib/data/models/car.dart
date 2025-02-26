638141b3988c4edad7649606499883ee9eaccbe8
Ryuuutaaa <rafiakmalwidikta@gmail.com>
2025-02-26 20:59:11 +0800

    Add Car model class with properties for model, distance, fuel capacity, and price per hour

---
+++ lib/data/models/car.dart
@@ -0,0 +1,8 @@
+class Car {
+  final String model;
+  final String distance;
+  final String fuelCapacity;
+  final String pricePerHour;
+
+  Car({required this.model, required this.distance, required this.fuelCapacity, required this.pricePerHour});
+}
