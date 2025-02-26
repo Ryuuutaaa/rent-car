7dec7ec2439c34ce8cef2bc637537f277f4e8433
Ryuuutaaa <rafiakmalwidikta@gmail.com>
2025-02-26 18:02:17 +0800

    Refactor onboarding page layout and update UI elements for improved user experience

--- lib/main.dart
+++ lib/main.dart
@@ -37,7 +37,6 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         title: 'Flutter Demo',
-        debugShowCheckedModeBanner: false,
         theme: ThemeData(
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
           useMaterial3: true,
--- lib/onboarding_page.dart
+++ lib/onboarding_page.dart
@@ -8,21 +8,55 @@ class OnboardingPage extends StatelessWidget {
     return Scaffold(
       body: Column(
         children: [
+          // Container(
+          //   decoration: BoxDecoration(
+          //     image: DecorationImage(
+          //       image: AssetImage('assets/onboarding.png'),
+          //       fit: BoxFit.cover,
+          //     ),
+          //   ),
+          // ),
           Container(
-            decoration: BoxDecoration(
-              image: DecorationImage(
-                image: AssetImage('assets/onboarding.png'),
-                fit: BoxFit.cover,
-              ),
-            ),
-          ),
-          Container(
+            margin: const EdgeInsets.all(20),
             child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Text(
+                const Text(
                   "Premium, \nEnjoy the luxury",
-                  style: TextStyle(color: Colors.white, fontSize: 32, fontWeight: FontWeight.bold),
+                  style: TextStyle(
+                    color: Colors.black54,
+                    fontSize: 32,
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
+                const SizedBox(
+                  height: 10,
+                ),
+                const Text(
+                  'Premium and prestige car daily rental. \nExperience the thrill at a lower price',
+                  style: TextStyle(
+                    color: Colors.grey,
+                    fontSize: 16,
+                  ),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                SizedBox(
+                  height: 54,
+                  width: 320,
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                      foregroundColor: Colors.black,
+                      backgroundColor: Colors.white,
+                    ),
+                    child: const Text(
+                      "Lets go",
+                      style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                )
               ],
             ),
           )
