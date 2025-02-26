28390dfa0bc364e30ecbfce6187bfcc718fed9c6
Ryuuutaaa <rafiakmalwidikta@gmail.com>
2025-02-26 22:50:15 +0800

    Add CarListScreen widget to display a list of cars

---
+++ lib/presentation/pages/car_list_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:rent_car_architecture/data/models/Car.dart';
+
+class CarListScreen extends StatelessWidget {
+  const CarListScreen({super.key}) : super(key: key);
+  final List<Car> cars = [
+    Car(model: "Fortuner GR", distance: 870, fuelCapacity: 50, pricePerHour: 45),
+    Car(model: "Fortuner GR", distance: 870, fuelCapacity: 50, pricePerHour: 45),
+    Car(model: "Fortuner GR", distance: 870, fuelCapacity: 50, pricePerHour: 45),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: ListView.builder(itemBuilder: itemBuilder),
+    );
+  }
+}
