401fafafc095c668c9e5eeb7fce0515be0783cfe
Ryuuutaaa <rafiakmalwidikta@gmail.com>
2025-02-26 20:50:39 +0800

    Update onboarding page background color and restructure layout for improved readability

--- lib/onboarding_page.dart
+++ lib/onboarding_page.dart
@@ -6,6 +6,7 @@ class OnboardingPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color(0xff2c2B34),
       body: Column(
         children: [
           Expanded(
@@ -19,48 +20,50 @@ class OnboardingPage extends StatelessWidget {
               ),
             ),
           ),
-          Container(
-            margin: const EdgeInsets.all(20),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  "Premium, \nEnjoy the luxury",
-                  style: TextStyle(
-                    color: Colors.black54,
-                    fontSize: 32,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                SizedBox(
-                  height: 10,
-                ),
-                Text(
-                  'Premium and prestige car daily rental. \nExperience the thrill at a lower price',
-                  style: TextStyle(
-                    color: Colors.grey,
-                    fontSize: 16,
-                  ),
-                ),
-                SizedBox(
-                  height: 20,
-                ),
-                SizedBox(
-                  height: 54,
-                  width: 320,
-                  child: ElevatedButton(
-                    onPressed: () {},
-                    style: ElevatedButton.styleFrom(
-                      foregroundColor: Colors.black,
-                      backgroundColor: Colors.white,
-                    ),
-                    child: Text(
-                      "Lets go",
-                      style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
+          Expanded(
+            child: Container(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "Premium, \nEnjoy the luxury",
+                    style: TextStyle(
+                      color: Colors.black54,
+                      fontSize: 32,
+                      fontWeight: FontWeight.bold,
                     ),
                   ),
-                )
-              ],
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Text(
+                    'Premium and prestige car daily rental. \nExperience the thrill at a lower price',
+                    style: TextStyle(
+                      color: Colors.grey,
+                      fontSize: 16,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  SizedBox(
+                    height: 54,
+                    width: 320,
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      style: ElevatedButton.styleFrom(
+                        foregroundColor: Colors.black,
+                        backgroundColor: Colors.white,
+                      ),
+                      child: Text(
+                        "Lets go",
+                        style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  )
+                ],
+              ),
             ),
           )
         ],
