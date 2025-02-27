e40a632deb08e5bfa35743c1e1118ab5f6e2312b
Ryuuutaaa <rafiakmalwidikta@gmail.com>
2025-02-28 02:28:55 +0800

    Refactor main.dart to improve MyApp initialization and ensure consistent use of const constructors

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
-import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
 import 'package:rent_car_architecture/presentation/pages/car_details_page.dart';
 import 'package:rent_car_architecture/presentation/pages/car_list_screen.dart';
 import 'package:rent_car_architecture/presentation/pages/onboarding_page.dart';
@@ -8,43 +8,22 @@ import 'package:rent_car_architecture/presentation/pages/onboarding_page.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: HomeScreen(),
-    );
-  }
-}
-
-class HomeScreen extends StatelessWidget {
-  final FirebaseFirestore db = FirebaseFirestore.instance;
-
-  void _incrementCounter() {
-    final user = {
-      "firstName": "Ryuta",
-      "lastName": "Hirarito",
-      "born": 2000
-    };
-
-    db.collection("users").add(user).then(
-          (DocumentReference doc) => print("User added with ID: ${doc.id}"),
-        );
-  }
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: OnboardingPage(),
+      home: const OnboardingPage(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
