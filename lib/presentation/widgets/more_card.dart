f2eb7658928ba75aec9534387dd0643d2bfba028
Ryuuutaaa <rafiakmalwidikta@gmail.com>
2025-02-28 01:58:26 +0800

    Add MoreCard widget for displaying car details in a structured layout

---
+++ lib/presentation/widgets/more_card.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:rent_car_architecture/data/models/Car.dart';
+
+class MoreCard extends StatelessWidget {
+  final Car car;
+
+  const MoreCard({super.key, required this.car});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Row(
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                car.model,
+                style: TextStyle(color: Colors.white, fontSize: 16, fontWeight: FontWeight.bold),
+              ),
+              SizedBox(
+                height: 5,
+              ),
+              Row(
+                children: [
+                  Icon(
+                    Icons.directions_car,
+                    color: Colors.white,
+                    size: 16,
+                  ),
+                  SizedBox(
+                    width: 5,
+                  ),
+                  Text(
+                    "> ${car.distance} km",
+                  )
+                ],
+              )
+            ],
+          ),
+          Icon(
+            Icons.arrow_forward_ios,
+            color: Colors.white,
+            size: 24,
+          ),
+        ],
+      ),
+    );
+  }
+}
