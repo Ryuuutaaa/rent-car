cda97333ab704eeeaaad5d4f9c2f99d0896e1bf5
Ryuuutaaa <rafiakmalwidikta@gmail.com>
2025-02-28 01:31:46 +0800

    Refactor CarDetailsPage layout to use SingleChildScrollView for improved scrolling and enhance user interface with better padding and styling

--- lib/presentation/pages/car_details_page.dart
+++ lib/presentation/pages/car_details_page.dart
@@ -17,48 +17,64 @@ class CarDetailsPage extends StatelessWidget {
           ],
         ),
       ),
-      body: Column(
-        children: [
-          CarCard(
-            car: Car(model: "Fortuner GR", distance: 870, fuelCapacity: 50, pricePerHour: 45),
-          ),
-          SizedBox(
-            height: 20,
-          ),
-          Container(
-            decoration: BoxDecoration(
-              color: Color(0xffF3F3F3),
-              borderRadius: BorderRadius.circular(20),
-              boxShadow: [
-                BoxShadow(color: Colors.black12, blurRadius: 10, spreadRadius: 5)
-              ],
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            CarCard(
+              car: Car(model: "Fortuner GR", distance: 870, fuelCapacity: 50, pricePerHour: 45),
             ),
-            child: Column(
-              children: [
-                CircleAvatar(
-                  radius: 40,
-                  backgroundImage: AssetImage('assets/user.ong'),
-                ),
-                SizedBox(
-                  height: 10,
-                ),
-                Text(
-                  'Jane Cooper',
-                  style: TextStyle(fontWeight: FontWeight.bold),
-                ),
-                Text(
-                  '\$4,253',
-                  style: TextStyle(color: Colors.grey),
-                ),
-              ],
+            SizedBox(
+              height: 20,
             ),
-          ),
-          Container(
-            decoration: BoxDecoration(borderRadius: BorderRadius.circular(20), image: DecorationImage(image: AssetImage('assets/maps.png'), fit: BoxFit.cover), boxShadow: [
-              BoxShadow(color: Colors.black12, blurRadius: 10, spreadRadius: 5)
-            ]),
-          ),
-        ],
+            Container(
+              decoration: BoxDecoration(
+                color: Color(0xffF3F3F3),
+                borderRadius: BorderRadius.circular(20),
+                boxShadow: [
+                  BoxShadow(color: Colors.black12, blurRadius: 10, spreadRadius: 5)
+                ],
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Column(
+                  children: [
+                    CircleAvatar(
+                      radius: 40,
+                      backgroundImage: AssetImage('assets/user.png'),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Text(
+                      'Jane Cooper',
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    Text(
+                      '\$4,253',
+                      style: TextStyle(color: Colors.grey),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Container(
+              height: 150,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                image: DecorationImage(
+                  image: AssetImage('assets/maps.png'),
+                  fit: BoxFit.cover,
+                ),
+                boxShadow: [
+                  BoxShadow(color: Colors.black12, blurRadius: 10, spreadRadius: 5)
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
