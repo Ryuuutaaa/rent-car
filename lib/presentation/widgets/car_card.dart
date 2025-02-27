a222661a9fe37efbb3f03d282daa0301c7d0d200
Ryuuutaaa <rafiakmalwidikta@gmail.com>
2025-02-27 11:57:00 +0800

    Enhance CarCard widget with improved styling and shadow effects for better visual appeal

--- lib/presentation/widgets/car_card.dart
+++ lib/presentation/widgets/car_card.dart
@@ -11,6 +11,18 @@ class CarCard extends StatelessWidget {
     return Container(
       margin: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
       padding: const EdgeInsets.all(20),
+      decoration: BoxDecoration(
+        color: const Color(0xfff3f3f3),
+        borderRadius: BorderRadius.circular(20),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.5),
+            spreadRadius: 1,
+            blurRadius: 5,
+            offset: const Offset(0, 3),
+          ),
+        ],
+      ),
       child: Column(
         children: [
           Image.asset(
