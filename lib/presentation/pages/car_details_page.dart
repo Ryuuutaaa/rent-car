d4fb679f38466bd5da6d8b7aac37aaea1ae6fbd1
Ryuuutaaa <rafiakmalwidikta@gmail.com>
2025-02-28 01:39:49 +0800

    Refactor CarDetailsPage layout for improved readability and user interface enhancements

--- lib/presentation/pages/car_details_page.dart
+++ lib/presentation/pages/car_details_page.dart
@@ -9,11 +9,11 @@ class CarDetailsPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Row(
+        title: Row(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Icon(Icons.info_outline),
-            Text(" Informatin"),
+            Text(" Information"),
           ],
         ),
       ),
@@ -21,55 +21,84 @@ class CarDetailsPage extends StatelessWidget {
         child: Column(
           children: [
             CarCard(
-              car: Car(model: "Fortuner GR", distance: 870, fuelCapacity: 50, pricePerHour: 45),
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            Container(
-              decoration: BoxDecoration(
-                color: const Color(0xffF3F3F3),
-                borderRadius: BorderRadius.circular(20),
-                boxShadow: [
-                  BoxShadow(color: Colors.black12, blurRadius: 10, spreadRadius: 5)
-                ],
-              ),
-              child: const Padding(
-                padding: EdgeInsets.all(20.0),
-                child: Column(
-                  children: [
-                    CircleAvatar(
-                      radius: 40,
-                      backgroundImage: AssetImage('assets/user.png'),
-                    ),
-                    SizedBox(
-                      height: 10,
-                    ),
-                    Text(
-                      'Jane Cooper',
-                      style: TextStyle(fontWeight: FontWeight.bold),
-                    ),
-                    Text(
-                      '\$4,253',
-                      style: TextStyle(color: Colors.grey),
-                    ),
-                  ],
-                ),
+              car: Car(
+                model: "Fortuner GR",
+                distance: 870,
+                fuelCapacity: 50,
+                pricePerHour: 45,
               ),
             ),
-            const SizedBox(
+            SizedBox(
               height: 20,
             ),
-            Container(
-              height: 150,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(20),
-                image: const DecorationImage(
-                  image: AssetImage('assets/maps.png'),
-                  fit: BoxFit.cover,
-                ),
-                boxShadow: [
-                  const BoxShadow(color: Colors.black12, blurRadius: 10, spreadRadius: 5)
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Container(
+                      padding: EdgeInsets.all(20),
+                      decoration: BoxDecoration(
+                        color: Color(0xffF3F3F3),
+                        borderRadius: BorderRadius.circular(20),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.black12,
+                            blurRadius: 10,
+                            spreadRadius: 5,
+                          )
+                        ],
+                      ),
+                      child: Padding(
+                        padding: EdgeInsets.all(20),
+                        child: Column(
+                          children: [
+                            CircleAvatar(
+                              radius: 40,
+                              backgroundImage: AssetImage('assets/user.png'),
+                            ),
+                            SizedBox(
+                              height: 10,
+                            ),
+                            Text(
+                              'Jane Cooper',
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            Text(
+                              '\$4,253',
+                              style: TextStyle(
+                                color: Colors.grey,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    width: 20,
+                  ),
+                  Expanded(
+                    child: Container(
+                      height: 150,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                        image: DecorationImage(
+                          image: AssetImage('assets/maps.png'),
+                          fit: BoxFit.cover,
+                        ),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.black12,
+                            blurRadius: 10,
+                            spreadRadius: 5,
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
                 ],
               ),
             ),
