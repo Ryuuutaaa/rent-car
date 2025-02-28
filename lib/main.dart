2b16ea6a50249953d7cb4180c3a62e4f6d3cb41d
Ryuuutaaa <rafiakmalwidikta@gmail.com>
2025-02-28 22:24:44 +0800

    Implement MapsDetailsPage with initial map setup and integrate it into MyApp

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:rent_car_architecture/presentation/pages/MapsDetailsPage.dart';
 import 'package:rent_car_architecture/presentation/pages/car_details_page.dart';
 import 'package:rent_car_architecture/presentation/pages/car_list_screen.dart';
 import 'package:rent_car_architecture/presentation/pages/onboarding_page.dart';
@@ -22,7 +23,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const OnboardingPage(),
+      home: const Mapsdetailspage(),
       debugShowCheckedModeBanner: false,
     );
   }
--- lib/presentation/pages/MapsDetailsPage.dart
+++ lib/presentation/pages/MapsDetailsPage.dart
@@ -1,11 +1,33 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart'; // Tambahkan ini
 
-class Mapsdetailspage extends StatelessWidget {
-  const Mapsdetailspage({super.key});
+class MapsDetailsPage extends StatelessWidget {
+  const MapsDetailsPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.red,
+        title: const Text("Maps Detail"),
+      ),
+      body: FlutterMap(
+        options: MapOptions(
+          initialCenter: LatLng(51.5, -0.09), // Gunakan initialCenter
+          initialZoom: 13.0, // Gunakan initialZoom
+        ),
+        children: [
+          TileLayer(
+            urlTemplate: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+            subdomains: [
+              'a',
+              'b',
+              'c'
+            ],
+          ),
+        ],
+      ),
+    );
   }
 }
