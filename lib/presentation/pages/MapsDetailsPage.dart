631c7862c77881795315b212d90e46f427e698f2
Ryuuutaaa <rafiakmalwidikta@gmail.com>
2025-02-28 22:26:47 +0800

    Enhance MapsDetailsPage title styling for better visibility

--- lib/presentation/pages/MapsDetailsPage.dart
+++ lib/presentation/pages/MapsDetailsPage.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
-import 'package:latlong2/latlong.dart'; // Tambahkan ini
+import 'package:latlong2/latlong.dart';
 
 class MapsDetailsPage extends StatelessWidget {
   const MapsDetailsPage({super.key});
@@ -10,12 +10,15 @@ class MapsDetailsPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.red,
-        title: const Text("Maps Detail"),
+        title: const Text(
+          "Maps Detail",
+          style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+        ),
       ),
       body: FlutterMap(
         options: MapOptions(
-          initialCenter: LatLng(51.5, -0.09), // Gunakan initialCenter
-          initialZoom: 13.0, // Gunakan initialZoom
+          initialCenter: LatLng(51.5, -0.09),
+          initialZoom: 13.0,
         ),
         children: [
           TileLayer(
