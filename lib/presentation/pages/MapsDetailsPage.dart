2cda0ef0b6db4d760a4da396c3018a6faa875c9f
Ryuuutaaa <rafiakmalwidikta@gmail.com>
2025-03-02 00:29:15 +0800

    Add feature icons to CardDetailCard for enhanced vehicle feature presentation

--- lib/presentation/pages/MapsDetailsPage.dart
+++ lib/presentation/pages/MapsDetailsPage.dart
@@ -160,3 +160,32 @@ Widget CardDetailCard() {
     ),
   );
 }
+
+Widget featureIcons() {
+  return Row(
+    mainAxisAlignment: MainAxisAlignment.spaceAround,
+    children: [
+      featureIcon(Icons.local_gas_station, "Diesel", "Common Rail");
+    ],
+  );
+}
+
+
+Widget featureIcon(IconData icon, String title, String subtitle) {
+  return Container(
+    width: 100,
+    height: 100,
+    padding: EdgeInsets.all(5),
+    decoration: BoxDecoration(
+      borderRadius: BorderRadius.circular(10),
+      border: Border.all(color: Colors.grey, width: 1)
+    ),
+    child: Column(
+      children: [
+        Icon( icon, size: 28,),
+        Text(title),
+        Text(subtitle, style: TextStyle(color: Colors.grey, fontSize: 10),),
+      ],
+    ),
+  );
+}
