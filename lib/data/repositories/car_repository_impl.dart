3fdc0332c14d499faea75bb768c319fb2b323f76
Ryuuutaaa <rafiakmalwidikta@gmail.com>
2025-03-03 03:18:35 +0800

    Implement fetchCars method in CarRepositoryImpl with unimplemented error

--- lib/data/repositories/car_repository_impl.dart
+++ lib/data/repositories/car_repository_impl.dart
@@ -1,8 +1,15 @@
 import 'package:rent_car_architecture/data/dataresources/firebase_car_data_source.dart';
+import 'package:rent_car_architecture/data/models/car.dart';
 import 'package:rent_car_architecture/domain/repositories/car_repository.dart';
 
 class CarRepositoryImpl implements CarRepository {
   final FirebaseCarDataSource dataSource;
 
   CarRepositoryImpl(this.dataSource);
+
+  @override
+  Future<List<Car>> fetchCars() {
+    // TODO: implement fetchCars
+    throw UnimplementedError();
+  }
 }
