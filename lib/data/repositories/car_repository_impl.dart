eae72cf2e3e7b21d42dc7659d0cf811a82e02034
Ryuuutaaa <rafiakmalwidikta@gmail.com>
2025-03-03 03:17:58 +0800

    Implement CarRepository interface with FirebaseCarDataSource integration

---
+++ lib/data/repositories/car_repository_impl.dart
@@ -0,0 +1,8 @@
+import 'package:rent_car_architecture/data/dataresources/firebase_car_data_source.dart';
+import 'package:rent_car_architecture/domain/repositories/car_repository.dart';
+
+class CarRepositoryImpl implements CarRepository {
+  final FirebaseCarDataSource dataSource;
+
+  CarRepositoryImpl(this.dataSource);
+}
