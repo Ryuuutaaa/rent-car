ce25af17d52ed7239b4f75fd9f5c15492b278ad5
Ryuuutaaa <rafiakmalwidikta@gmail.com>
2025-03-04 03:14:11 +0800

    Register GetCars use case and CarBloc in the service locator

--- lib/injection_container.dart
+++ lib/injection_container.dart
@@ -2,6 +2,9 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:get_it/get_it.dart';
 import 'package:rent_car_architecture/data/dataresources/firebase_car_data_source.dart';
 import 'package:rent_car_architecture/data/repositories/car_repository_impl.dart';
+import 'package:rent_car_architecture/domain/repositories/car_repository.dart';
+import 'package:rent_car_architecture/domain/usecases/get_cars.dart';
+import 'package:rent_car_architecture/presentation/bloc/car_bloc.dart';
 
 GetIt getIt = GetIt.instance;
 
@@ -10,6 +13,8 @@ void initInjection() {
     getIt.registerLazySingleton<FirebaseFirestore>(() => FirebaseFirestore.instance);
     getIt.registerLazySingleton<FirebaseCarDataSource>(() => FirebaseCarDataSource(firestore: getIt<FirebaseFirestore>));
     getIt.registerLazySingleton<CarRepositoryImpl>(() => CarRepositoryImpl(getIt<FirebaseCarDataSource>()));
+    getIt.registerLazySingleton<GetCars>(() => GetCars(getIt<CarRepository>()));
+    getIt.registerFactory<GetCars>(() => CarBloc(getCars: getIt<GetCars>()));
   } catch (e) {
     throw e;
   }
