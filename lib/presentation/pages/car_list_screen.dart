ed0db1c9f282fe6119073d740e7433a6c9598a2d
Ryuuutaaa <rafiakmalwidikta@gmail.com>
2025-03-04 03:27:27 +0800

    Implement Bloc pattern in CarListScreen for state management and loading indicators

--- lib/presentation/pages/car_list_screen.dart
+++ lib/presentation/pages/car_list_screen.dart
@@ -1,14 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:rent_car_architecture/data/models/Car.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:rent_car_architecture/presentation/bloc/car_bloc.dart';
+import 'package:rent_car_architecture/presentation/bloc/car_state.dart';
 import 'package:rent_car_architecture/presentation/widgets/car_card.dart';
 
 class CarListScreen extends StatelessWidget {
-  final List<Car> cars = [
-    Car(model: "Fortuner GR", distance: 870, fuelCapacity: 50, pricePerHour: 45),
-    Car(model: "Fortuner GR", distance: 870, fuelCapacity: 50, pricePerHour: 45),
-    Car(model: "Fortuner GR", distance: 870, fuelCapacity: 50, pricePerHour: 45),
-  ];
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,12 +13,23 @@ class CarListScreen extends StatelessWidget {
         backgroundColor: Colors.white,
         foregroundColor: Colors.black,
       ),
-      body: ListView.builder(
-        itemCount: cars.length,
-        itemBuilder: (context, index) {
-          return CarCard(
-            car: cars[index],
-          );
+      body: BlocBuilder<CarBloc, CarState>(
+        builder: (context, state) {
+          if (state is CarsLoading) {
+            return const Center(child: CircularProgressIndicator());
+          } else if (state is CarsLoaded) {
+            return ListView.builder(
+              itemCount: state.cars.length,
+              itemBuilder: (context, index) {
+                return CarCard(
+                  car: state.cars[index],
+                );
+              },
+            );
+          } else if (state is CarsError) {
+            return Center(child: Text("Error: ${state.message}"));
+          }
+          return Container();
         },
       ),
     );
