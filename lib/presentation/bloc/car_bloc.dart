1d9a77c263d4531e8e41c55a125c26e7f4295ee5
Ryuuutaaa <rafiakmalwidikta@gmail.com>
2025-03-04 03:01:58 +0800

    Fix CarBloc implementation to extend Bloc and correct async event handling

--- lib/presentation/bloc/car_bloc.dart
+++ lib/presentation/bloc/car_bloc.dart
@@ -1,18 +1,21 @@
 import 'package:rent_car_architecture/presentation/bloc/car_event.dart';
 import 'package:rent_car_architecture/presentation/bloc/car_state.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:rent_car_architecture/domain/usecases/get_cars.dart';
 
-class CarBloc Bloc<CarEvent, CarState> {
+class CarBloc extends Bloc<CarEvent, CarState> {
   final GetCars getCars;
 
-  CarBloc({required this.getCars}) : super(CarsLoading()){
-    on<LoadCars>((event, emit)) asych {
+  CarBloc({required this.getCars}) : super(CarsLoading()) {
+    on<LoadCars>((event, emit) async {
       emit(CarsLoading());
-      try{
+      try {
         final cars = await getCars();
         emit(CarsLoaded(cars));
-      }catch(e){
+      } catch (e) {
         emit(CarsError(e.toString()));
       }
-    }
+    });
   }
-};
+}
+ 
