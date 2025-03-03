a166082577d4f03e0b6bd267c7d11794be26d70c
Ryuuutaaa <rafiakmalwidikta@gmail.com>
2025-03-04 03:12:05 +0800

    Add Firebase dependencies and initialize service locator for car data source and repository

--- lib/injection_container.dart
+++ lib/injection_container.dart
@@ -1,3 +1,16 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:get_it/get_it.dart';
+import 'package:rent_car_architecture/data/dataresources/firebase_car_data_source.dart';
+import 'package:rent_car_architecture/data/repositories/car_repository_impl.dart';
 
 GetIt getIt = GetIt.instance;
+
+void initInjection() {
+  try {
+    getIt.registerLazySingleton<FirebaseFirestore>(() => FirebaseFirestore.instance);
+    getIt.registerLazySingleton<FirebaseCarDataSource>(() => FirebaseCarDataSource(firestore: getIt<FirebaseFirestore>));
+    getIt.registerLazySingleton<CarRepositoryImpl>(() => CarRepositoryImpl(getIt<FirebaseCarDataSource>()));
+  } catch (e) {
+    throw e;
+  }
+}
