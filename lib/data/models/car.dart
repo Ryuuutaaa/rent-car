dfec47ff1610b735acbee6b87b3db68bf33ed82d
Ryuuutaaa <rafiakmalwidikta@gmail.com>
2025-03-04 03:41:40 +0800

    Add ID field to Car model and update fromMap factory method for Firestore integration

--- lib/data/models/car.dart
+++ lib/data/models/car.dart
@@ -1,12 +1,34 @@
 class Car {
+  final String id; // Add ID for Firestore document reference
   final String model;
   final double distance;
   final double fuelCapacity;
   final double pricePerHour;
 
-  Car({required this.model, required this.distance, required this.fuelCapacity, required this.pricePerHour});
+  Car({
+    this.id = '', // Default empty ID for new cars
+    required this.model,
+    required this.distance,
+    required this.fuelCapacity,
+    required this.pricePerHour,
+  });
 
-  factory Car.fromMap(Map<String, dynamic> map) {
-    return Car(model: map['model'], distance: map['distance'], fuelCapacity: map['fuelCapacity'], pricePerHour: map['pricePerHour']);
+  factory Car.fromMap(Map<String, dynamic> map, {String id = ''}) {
+    return Car(
+      id: id,
+      model: map['model'] ?? '',
+      distance: (map['distance'] ?? 0).toDouble(),
+      fuelCapacity: (map['fuelCapacity'] ?? 0).toDouble(),
+      pricePerHour: (map['pricePerHour'] ?? 0).toDouble(),
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'model': model,
+      'distance': distance,
+      'fuelCapacity': fuelCapacity,
+      'pricePerHour': pricePerHour,
+    };
   }
 }
